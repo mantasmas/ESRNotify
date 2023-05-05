59c9650577c44af47664da2ade7a5695dc76adb1
Zymantas Maumevicius <z.maumevicius@gmail.com>
2023-05-05 17:47:35 +0300

    Add logout to settings

--- src/features/settings/SettingScreen.tsx
+++ src/features/settings/SettingScreen.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native-paper';
-import { View } from 'react-native';
+import styled from '@emotion/native';
+import { Button, Text } from 'react-native-paper';
+import { useAuth } from '../../providers/auth.provider';
+
+const Container = styled.SafeAreaView`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  /* justify-content: center; */
+  flex-grow: 1;
+  margin: 16px 16px;
+`;
+
+const ContainerSpaced = styled(Container)`
+  justify-content: space-between;
+`;
 
 export const SettingsScreen = () => {
+  const ctx = useAuth();
+
+  const logout = ctx?.signout;
+
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <ContainerSpaced>
+      <Container>
+        <Text>Settings</Text>
+        <Text>Settings</Text>
+        <Text>Settings</Text>
+      </Container>
+      <Button textColor="red" onPress={logout}>
+        Logout
+      </Button>
+    </ContainerSpaced>
   );
 };
